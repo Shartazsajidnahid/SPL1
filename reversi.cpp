b082562c9f77ca02642abf51ddfcfd4e63b623b0
Shartazsajidnahid <48786813+Shartazsajidnahid@users.noreply.github.com>
2020-03-10 02:08:14 +0600

    Add files via upload
    Show playable options

--- reversi.cpp
+++ reversi.cpp
@@ -4,9 +4,18 @@
 
 #define black 1
 #define red (-1)
+#define PLAYABLE 2
+#define EMPTY 0
+#define TRUE 1
+#define FALSE 0
 
 using namespace std;
 int board[8][8]= {0}, player;
+int playable_direction[8][8][8];
+int game_ended = FALSE;
+int skipped_turn = FALSE;
+int wrong_move = FALSE;
+int has_valid_move = FALSE;
 
 void initial_board()
 {
@@ -17,7 +26,144 @@ void initial_board()
     player = black;
 }
 
+int is_valid_position( int i, int j )
+{
+    if ( i < 0 || i >= 8 || j < 0 || j >= 8 ) return FALSE;
+    return TRUE;
+}
 
+int distance( int i1, int j1, int i2, int j2 )
+{
+    int di = abs( i1 - i2 ), dj = abs( j1 - j2 );
+    if ( di > 0 ) return di;
+    return dj;
+}
+
+int is_playable( int i, int j )
+{
+    memset( playable_direction[i][j], 0, 8 );
+    if ( !is_valid_position( i, j ) ) return FALSE;
+    if ( board[i][j] != EMPTY ) return FALSE;
+    int playable = FALSE;
+
+    int opposing_player = player*(-1);
+
+    // Test UL diagonal
+    int i_it = i-1, j_it = j-1;
+    while ( is_valid_position( i_it, j_it ) && board[i_it][j_it] == opposing_player )
+    {
+        i_it -= 1;
+        j_it -= 1;
+    }
+    if ( is_valid_position( i_it, j_it ) && distance( i, j, i_it, j_it ) > 1 && board[i_it][j_it] == player )
+    {
+        playable_direction[i][j][0] = 1;
+        playable = TRUE;
+    }
+
+    // Test UP path
+    i_it = i-1, j_it = j;
+    while ( is_valid_position( i_it, j_it ) && board[i_it][j_it] == opposing_player )
+        i_it -= 1;
+
+    if ( is_valid_position( i_it, j_it ) && distance( i, j, i_it, j_it ) > 1 && board[i_it][j_it] == player )
+    {
+        playable_direction[i][j][1] = 1;
+        playable = TRUE;
+    }
+
+    // Test UR diagonal
+    i_it = i-1, j_it = j+1;
+    while ( is_valid_position( i_it, j_it ) && board[i_it][j_it] == opposing_player )
+    {
+        i_it -= 1;
+        j_it += 1;
+    }
+    if ( is_valid_position( i_it, j_it ) && distance( i, j, i_it, j_it ) > 1 && board[i_it][j_it] == player )
+    {
+        playable_direction[i][j][2] = 1;
+        playable = TRUE;
+    }
+
+    // Test LEFT path
+    i_it = i, j_it = j-1;
+    while ( is_valid_position( i_it, j_it ) && board[i_it][j_it] == opposing_player )
+        j_it -= 1;
+
+    if ( is_valid_position( i_it, j_it ) && distance( i, j, i_it, j_it ) > 1 && board[i_it][j_it] == player )
+    {
+        playable_direction[i][j][3] = 1;
+        playable = TRUE;
+    }
+
+    // Test RIGHT path
+    i_it = i, j_it = j+1;
+    while ( is_valid_position( i_it, j_it ) && board[i_it][j_it] == opposing_player )
+        j_it += 1;
+
+    if ( is_valid_position( i_it, j_it ) && distance( i, j, i_it, j_it ) > 1 && board[i_it][j_it] == player )
+    {
+        playable_direction[i][j][4] = 1;
+        playable = TRUE;
+    }
+
+    // Test DL diagonal
+    i_it = i+1, j_it = j-1;
+    while ( is_valid_position( i_it, j_it ) && board[i_it][j_it] == opposing_player )
+    {
+        i_it += 1;
+        j_it -= 1;
+    }
+    if ( is_valid_position( i_it, j_it ) && distance( i, j, i_it, j_it ) > 1 && board[i_it][j_it] == player )
+    {
+        playable_direction[i][j][5] = 1;
+        playable = TRUE;
+    }
+
+    // Test DOWN path
+    i_it = i+1, j_it = j;
+    while ( is_valid_position( i_it, j_it ) && board[i_it][j_it] == opposing_player )
+        i_it += 1;
+
+    if ( is_valid_position( i_it, j_it ) && distance( i, j, i_it, j_it ) > 1 && board[i_it][j_it] == player )
+    {
+        playable_direction[i][j][6] = 1;
+        playable = TRUE;
+    }
+
+    // Test DR diagonal
+    i_it = i+1, j_it = j+1;
+    while ( is_valid_position( i_it, j_it ) && board[i_it][j_it] == opposing_player )
+    {
+        i_it += 1;
+        j_it += 1;
+    }
+    if ( is_valid_position( i_it, j_it ) && distance( i, j, i_it, j_it ) > 1 && board[i_it][j_it] == player )
+    {
+        playable_direction[i][j][7] = 1;
+        playable = TRUE;
+    }
+    return playable;
+}
+
+
+void mark_playable_positions()
+{
+    has_valid_move = FALSE;
+    for ( int i=0; i<8; ++i )
+    {
+        for ( int j=0; j<8; ++j )
+        {
+            if ( board[i][j] == PLAYABLE )
+                board[i][j] = EMPTY;
+            if ( is_playable( i, j ) )
+            {
+                board[i][j] = PLAYABLE;
+                has_valid_move = TRUE;
+            }
+        }
+    }
+}
 
 
 int main()
@@ -61,7 +207,16 @@ int main()
         {
             for(int j = 0; j<8; j++)
             {
-                if(board[i][j]==black || board[i][j]== red )
+                cout << board[i][j] << "  ";
+            }
+        cout << endl;
+        }
+
+        for(int i =0; i<8; i++)
+        {
+            for(int j = 0; j<8; j++)
+            {
+                if(board[i][j]==black || board[i][j]== red || board[i][j] == PLAYABLE)
                 {
                     x=i+1;
                     y=j+1;
@@ -74,19 +229,43 @@ int main()
                         floodfill(y,x,WHITE);
                     }
                     else if(board[i][j]==red)
-                    {
+                    {   cout << "FOUND RED";
                         setfillstyle(SOLID_FILL, RED);
                         circle(y, x , 20);
                         floodfill(y,x,WHITE);
                     }
+                    else if (board[i][j]==PLAYABLE){
+                        x = i+1;
+                        y = j+1;
+                        outtextxy((x*50) + 20, (y*50) + 20, "+");
+                    }
                 }
+               else{
+                x = i+1;
+                y = j+1;
+                left = y*50;
+                right = left+50;
+                top = x*50;
+                bottom = top+50;
+                setfillstyle(SOLID_FILL,GREEN);
+                rectangle(left,top,right,bottom);
+
+                floodfill((left+25),(top+25),WHITE);
+                }
+
+
             }
         }
-        player = player*(-1);
-        cin >> x >> y;
-        board[x-1][y-1] = player;
+        mark_playable_positions();
+        cin >> x>>y;
+        board[x-1][y-1] = EMPTY;
+
+       // player = player*(-1);
+       // cin >> x >> y;
+        //board[x-1][y-1] = player;
 
     }
+
     getch();
 
 
